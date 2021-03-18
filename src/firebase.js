82413d468400f7ae9c91f27f0ec3dd6fb55bb76b
Scottly426 <lyscott123@gmail.com>
2021-03-18 09:36:07 -0700

    adding finished project

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCE35OEeTGBw6HsVkGl73xnCY989znJ4Ys",
-  authDomain: "discord-clone-yt.firebaseapp.com",
-  databaseURL: "https://discord-clone-yt.firebaseio.com",
-  projectId: "discord-clone-yt",
-  storageBucket: "discord-clone-yt.appspot.com",
-  messagingSenderId: "1009639771910",
-  appId: "1:1009639771910:web:ded4a3b3f26d44a4489887",
-  measurementId: "G-HJ43RQR7D2",
+  apiKey: "AIzaSyCBxya_Ad61A1niWJobgCccY22H9_3ymkM",
+  authDomain: "discord-clone-11a11.firebaseapp.com",
+  projectId: "discord-clone-11a11",
+  storageBucket: "discord-clone-11a11.appspot.com",
+  messagingSenderId: "762848095174",
+  appId: "1:762848095174:web:58001a6ae0d076832c20c7",
+  measurementId: "G-6SP4MY2K2R"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
